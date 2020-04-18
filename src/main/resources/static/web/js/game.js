5aa5f8405ea2fd1fee1dacedae11d0a65e9268be
wignaciow <ing.ignacio.elizalde@gmail.com>
2020-04-18 16:46:42 -0300

    Java task 3, 3 ships placement - half

--- src/main/resources/static/web/js/game.js
+++ src/main/resources/static/web/js/game.js
@@ -1,9 +1,6 @@
 var vue = new Vue({
    el: '#app',
    data: {
-        gridNumbers: ["#", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
-        gridLetters: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"],
-        
         gpId: null,
         gpInfo: [],
         player: [],
@@ -42,13 +39,13 @@ var vue = new Vue({
             })   
         },
         gpShipsLocation: function () {
-            vue.gpInfo.ships.forEach(ship => {
-            ship.locations.forEach( x => {
-                document.getElementById(x).classList.add("bg-secondary");
-                vue.shipsLocation.push(x);
+            vue.gpInfo.ships.forEach(x => {
+            x.locations.forEach( y => {
+                document.getElementById(y).classList.add("bg-secondary");
+                vue.shipsLocation.push(y);
              })
              })
-         },
+         },  
         gpPlayerSalvoLocation: function () {
             for (var i = 0; i < vue.gpInfo.salvos.length; i++) {
                 if (vue.gpInfo.salvos[i].playerId == vue.player.id) {
@@ -81,46 +78,129 @@ var vue = new Vue({
                 vue.opponent =  vue.gpInfo.gamePlayers[i].player;
                 }
             }
-        }
+        },
     }
 })
 
 vue.obtainGpId();
 vue.gpInformation();
 
+/*GRILLA*/
+//todas las funciones se encuentran en la documentación
+//https://github.com/gridstack/gridstack.js/tree/develop/doc
 
-/*var Ships = [
-    {
-        "id" : 1,
-        "name" : "Aircraft",
-        "size": 5,
-        "image": 1,
-    },
-    {
-        "id" : 1,
-        "name" : "Aircraft",
-        "size" : 0,
-        "image" : 0,
-    },
-    {
-        "id" : 1,
-        "name" : "Aircraft",
-        "size" : 0,
-        "image" : 0,
-    },
-    {
-        "id" : 1,
-        "name" : "Aircraft",
-        "size" : 0,
-        "image" : 0,
-    },
-    {
-        "id" : 1,
-        "name" : "Aircraft",
-        "size" : 0,
-        "image" : 0,
+const optionsC = {
+    //grilla de 10 x 10
+    column: 6,
+    row: 5,
+    //separacion entre elementos (les llaman widgets)
+    verticalMargin: 0,
+    //altura de las celdas
+    disableOneColumnMode: true,
+    //altura de las filas/celdas
+    cellHeight: 70,
+    //necesario
+    float: true,
+    //desabilitando el resize de los widgets
+    disableResize: true,
+    //false permite mover los widgets, true impide
+    staticGrid: false,
+}
+
+//Iniciando la grilla en modo libe staticGridFalse
+const gridChoose = GridStack.init(optionsC, '#gridChoose');
+//obteniendo los ships agregados en la grilla
+
+//Agregando elementos (widget) desde el javascript
+//elemento,x,y,width,height
+gridChoose.addWidget('<div><div id="carrier" class="grid-stack-item-content carrierHorizontal"></div><div/>',
+1, 0, 5, 1,);
+
+gridChoose.addWidget('<div><div id="battleship" class="grid-stack-item-content battleshipHorizontal"></div><div/>',
+1, 1, 4, 1);
+
+gridChoose.addWidget('<div><div id="submarine" class="grid-stack-item-content submarineHorizontal"></div><div/>',
+1, 2, 3, 1);
+
+gridChoose.addWidget('<div><div id="destroyer" class="grid-stack-item-content destroyerHorizontal"></div><div/>',
+1, 3, 3, 1);
+
+gridChoose.addWidget('<div><div id="patrol" class="grid-stack-item-content patrolHorizontal"></div><div/>',
+1, 4, 2, 1);
+
+
+const optionsP = {
+    //grilla de 10 x 10
+    column: 10,
+    row: 10,
+    //separacion entre elementos (les llaman widgets)
+    verticalMargin: 0,
+    //altura de las celdas
+    disableOneColumnMode: true,
+    //altura de las filas/celdas
+    cellHeight: 52,
+    //necesario
+    float: true,
+    //desabilitando el resize de los widgets
+    disableResize: true,
+    //false permite mover los widgets, true impide
+    staticGrid: false,
+    // function example, else can be simple: true | false | '.someClass' value
+    acceptWidgets: function(i, el) { return true; } 
+}
+
+
+
+//iniciando la grilla en modo libe statidGridFalse
+const gridPosition = GridStack.init(optionsP, '#gridPosition');
+
+gridPosition.on('dropped', function(event, previousWidget, newWidget) {
+    
+    newWidget.el.onclick = function(event){
+        //obteniendo el ship (widget) al que se le hace click
+        let itemContent = event.target;
+        //obteniendo valores del widget
+        let itemX = parseInt(itemContent.parentElement.dataset.gsX);
+        let itemY = parseInt(itemContent.parentElement.dataset.gsY);
+        let itemWidth = parseInt(itemContent.parentElement.dataset.gsWidth);
+        let itemHeight = parseInt(itemContent.parentElement.dataset.gsHeight);
+        //si esta horizontal se rota a vertical sino a horizontal
+        if(itemContent.classList.contains(itemContent.id + 'Horizontal')){
+            //veiricando que existe espacio disponible para la rotacion
+            if(gridPosition.isAreaEmpty(itemX, itemY + 1 , itemHeight, itemWidth - 1) && (itemY + (itemWidth - 1) <= 9 ) ){
+                //la rotacion del widget es simplemente intercambiar el alto y ancho del widget, ademas se cambia la clase
+                gridPosition.resize(itemContent.parentElement, itemHeight, itemWidth);
+                itemContent.classList.remove(itemContent.id + 'Horizontal');
+                itemContent.classList.add(itemContent.id + 'Vertical');
+            } else {
+                alert("Espacio no disponible");
+            }
+        } else {
+            if(gridPosition.isAreaEmpty(itemX + 1, itemY , itemHeight - 1, itemWidth)  && (itemX + (itemHeight - 1) <= 9 )){
+                gridPosition.resize(itemContent.parentElement, itemHeight, itemWidth);
+                itemContent.classList.remove(itemContent.id + 'Vertical');
+                itemContent.classList.add(itemContent.id + 'Horizontal');
+            }else{
+                alert("Espacio no disponible");
+            }
+        }
     }
-]*/
+  console.log('Added widget in dropped grid:', newWidget);
+});
+
+
+
+
+
+
+  
+    
+    
+
+
+
+
+
 
 
 
--- src/main/resources/static/web/js/games.js
+++ src/main/resources/static/web/js/games.js
@@ -3,26 +3,62 @@ var vue = new Vue({
     data: {
         show: "home",
         gamesInfo: [],
+        returnGameList: [],
+        joinGameList: [],
         gamePlayersInfo: [],
         gameFinish: [],
         players: [],
-        user: "",
+        userN: "",
         userNick: "",
-        userName: "",
-        password: "",
-        nickName: "",
+        nickNameV: "",
+        userNameV: "",
+        passwordV: "",
+        nickNameM: "",
+        userNameM: "",
+        passwordM: "",
+        
     },
     methods: {
         display: function (page) {
             this.show = page;
         },
-        logIn: function () {
-            if (vue.userName == "" || vue.password == "") {
+        signUpV: function () {
+            $.post("/api/players", {
+                    nickName: vue.nickNameV,
+                    userName: vue.userNameV,
+                    password: vue.passwordV
+                })
+                .done(function () {
+                    alert("Sign up successful!");
+                    vue.logInV();
+                })
+                .fail(function () {
+                    alert("Wrong user name or password, Try Sign up again!");
+                    location.reload();
+                })
+        },
+        signUpM: function () {
+            $.post("/api/players", {
+                    nickName: vue.nickNameM,
+                    userName: vue.userNameM,
+                    password: vue.passwordM
+                })
+                .done(function () {
+                    alert("Sign up successful!");
+                    vue.logInM();
+                })
+                .fail(function () {
+                    alert("Wrong user name or password, Try Sign up again!");
+                    location.reload();
+                })
+        },
+        logInV: function () {
+            if (vue.userNameV == "" || vue.passwordV == "") {
                 alert("Please complete all fields");
             } else {
                 $.post("/api/login", {
-                        userName: vue.userName,
-                        password: vue.password
+                        userName: vue.userNameV,
+                        password: vue.passwordV
                     }).done(function () {
                         location.reload();
                         alert("Log in successful");
@@ -34,45 +70,64 @@ var vue = new Vue({
                     })
             }
         },
-        signUp: function () {
-            $.post("/api/players", {
-                    nickName: vue.nickName,
-                    userName: vue.userName,
-                    password: vue.password
-                })
-                .done(function () {
-                    alert("Sign up successful!");
-                    vue.logIn();
-                })
-                .fail(function () {
-                    alert("Wrong user name or password, Try Sign up again!");
-                    location.reload();
-                })
+        logInM: function () {
+            if (vue.userNameM == "" || vue.passwordM == "") {
+                alert("Please complete all fields");
+            } else {
+                $.post("/api/login", {
+                        userName: vue.userNameM,
+                        password: vue.passwordM
+                    }).done(function () {
+                        location.reload();
+                        alert("Log in successful");
+
+                    })
+                    .fail(function () {
+                        alert("Wrong user name or password, Please try again or Sign up");
+                        location.reload();
+                    })
+            }
         },
-        loginFormTab: function (e) {
+        loginFormTabV: function (e) {
             e.preventDefault();
 
-            document.getElementById('logLi').classList.remove('active');
-            document.getElementById('signLi').classList.remove('active');
+            document.getElementById('logLiV').classList.remove('active');
+            document.getElementById('signLiV').classList.remove('active');
 
             document.getElementById(e.target.id).parentElement.classList.add('active');
 
             target = e.target.hash.substring(1);
 
-            document.getElementById('login').style.display = "none";
-            document.getElementById('signup').style.display = "none";
+            document.getElementById('loginV').style.display = "none";
+            document.getElementById('signupV').style.display = "none";
+
+            document.getElementById(target).style.display = "block";
+        },
+        loginFormTabM: function (e) {
+            e.preventDefault();
+
+            document.getElementById('logLiM').classList.remove('active');
+            document.getElementById('signLiM').classList.remove('active');
+
+            document.getElementById(e.target.id).parentElement.classList.add('active');
+
+            target = e.target.hash.substring(1);
+
+            document.getElementById('loginM').style.display = "none";
+            document.getElementById('signupM').style.display = "none";
 
             document.getElementById(target).style.display = "block";
         },
         actualUser: function () {
             $.getJSON("/api/games", function (data) {
                 if (data.playerLogged !== null) {
-                    vue.user = data.playerLogged.userName;
+                    vue.userN = data.playerLogged.userName;
                     vue.userNick = data.playerLogged.nickname;
                 } else {
-                    vue.user = null;
+                    vue.userN = null;
                     vue.userNick = null;
                 }
+                vue.gameList();
             })
         },
         logOut: function () {
@@ -83,8 +138,10 @@ var vue = new Vue({
         gamesData: function () {
             $.getJSON("/api/games", function (data) {
                 vue.gamesInfo = data.games;
+                vue.actualUser();
                 vue.gamesPlayerData();
                 vue.playerData();
+                
             })
         },
         gamesPlayerData: function () {
@@ -133,16 +190,28 @@ var vue = new Vue({
         createGame: function () {
             $.post("/api/games")
                 .done(function (data) {
-                    window.open("game.html?gp=" + data.gpID, "_blank");
+                    window.open("game.html?gp=" + data.gamePlayerId, "_blank");
                 })
         },
         joinGame: function (gameID) {
             $.post("/api/games/" + gameID + "/player")
                 .done(function (data) {
-                    window.open("game.html?gp=" + data.gpID, "_blank");
+                    window.open("game.html?gp=" + data.gamePlayerId, "_blank");
                 })
+        },
+        gameList: function () {
+            vue.gamesInfo.forEach(gal => {
+                gal.gamePlayers.forEach(gpl => {
+                    if (gpl.player.userName == vue.userN) {
+                        vue.returnGameList.push(gal)
+                    } else if (gal.gamePlayers.length == 1 && gpl.player.userName != vue.userN) {
+                        vue.joinGameList.push(gal)
+                    };
+                })
+            })
         },
     }
 })
 vue.gamesData();
-vue.actualUser();
+
+
